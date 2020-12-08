cc769e51b18165490ba772693e532cb759425aee
matihan <mtynmnswry092@gmail.com>
2020-12-08 08:42:48 +0330

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let imgElement = document.getElementById("srcImage");
+let inputElement = document.getElementById("fileInput");
+inputElement.addEventListener(
+    "change",
+    (e) => {
+        imgElement.src = URL.createObjectURL(e.target.files[0]);
+    },
+    false
+);
+imgElement.onload = function () {
+    let mat = cv.imread(imgElement);
+    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY);
+    cv.imshow("outputCanvas", mat);
+    mat.delete();
+};
+
+function opencvIsReady() {
+    document.getElementById("status").innerHTML =
+        "OpenCV.js (WebAssembly) is ready.";
+}
